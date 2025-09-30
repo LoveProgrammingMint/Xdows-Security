4387014f40fbff387e5ec485fe2f294aae81a3ae
XTY64XTY <a1b2c3ABC1231@outlook.com>
2025-09-30 22:36:22 +0800

    优化代码文件

--- Xdows-Security/InterceptWindow.xaml.cs
+++ Xdows-Security/InterceptWindow.xaml.cs
@@ -1,26 +1,8 @@
-using Microsoft.Diagnostics.Tracing.StackSources;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Xdows.ScanEngine;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Xdows_Security
 {
     public sealed partial class InterceptWindow : Window
@@ -33,7 +15,7 @@ namespace Xdows_Security
             var w = new InterceptWindow();
             try { w.Activate(); } catch { }
             w.SetFileInfo(path);
-            //SetIcon("logo.ico");
+            // SetIcon("logo.ico");
         }
 
         private void SetFileInfo(string path)

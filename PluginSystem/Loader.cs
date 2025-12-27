453b58f4a648d549f6c0fa0123cd93c4593ed66b
LoveProgrammingMint <mint121986@126.com>
2025-12-27 14:57:28 +0800

    完成最新插件系统加载部分（保留旧的）

---
+++ PluginSystem/Loader.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PluginSystem
+{
+    
+    public class Config
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("NameSpase")]
+        public string NameSpase { get; set; }
+
+        [JsonPropertyName("EntryAddress")]
+        public string EntryAddress { get; set; }
+
+        [JsonPropertyName("EntryFunction")]
+        public string EntryFunction { get; set; }
+    }
+    public enum LoadState
+    {
+        None,
+        NotFound,
+        Borken
+    }
+
+    internal class Loader
+    {
+        Config config;
+        LoadState state;
+
+        public Grid GetGrid(Assembly asm)
+        {
+            Type t = asm.GetType(config.EntryAddress);
+
+            object obj = Activator.CreateInstance(t);
+
+            MethodInfo mi = t.GetMethod(config.EntryFunction);
+            return (Grid)mi.Invoke(obj, []);
+        }
+
+        public Assembly Load(string Name)
+        {
+            if (!File.Exists($".\\Plugin\\{Name}\\Plugin.dll")) state = LoadState.Borken;
+
+            try
+            {
+                Assembly asm = Assembly.LoadFrom($".\\Plugin\\{Name}\\Plugin.dll");
+                return asm;
+            }
+            catch (Exception)
+            {
+                state = LoadState.Borken;
+            }
+            return null;
+        }
+
+        public void LoadConfig(string Name)
+        {
+            if (string.IsNullOrEmpty(Name)) state = LoadState.NotFound;
+
+            if (!File.Exists($".\\Plugin\\{Name}")) state = LoadState.NotFound;
+
+            try
+            {
+                string json = File.ReadAllText($".\\Plugin\\{Name}\\Information.json");
+                config = JsonSerializer.Deserialize<Config>(json);
+            }
+            catch (Exception)
+            {
+                state = LoadState.Borken;
+            }
+        }
+    }
+}
